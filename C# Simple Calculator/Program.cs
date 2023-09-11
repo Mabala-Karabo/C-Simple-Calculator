68ad542c8f4de0197fb59bddd44c67a5200805ec
Mabala-Karabo <mabalakarabo25@gmail.com>
2023-09-11 15:01:46 +0200

    Add project files.

---
+++ C# Simple Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num1 = Convert.ToInt32("45");
+           // Console.WriteLine(num1);
+            Console.WriteLine("Enter a first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(num1 + " + " + num2 + " = " + (num1 + num2));
+            Console.WriteLine(num1 + " - " + num2 + " = " + (num1 - num2));
+            Console.WriteLine(num1 + " * " + num2 + " = " + (num1 * num2));
+            Console.WriteLine(num1 + " / " + num2 + " = " + (num1 / num2));
+            Console.WriteLine(num1 + " mod " + num2 + " = " + (num1 % num2) + " That is the remainder of " + num1 + " and " + num2);
+
+            //freeeze the console
+            Console.ReadLine();
+
+        }
+    }
+}
